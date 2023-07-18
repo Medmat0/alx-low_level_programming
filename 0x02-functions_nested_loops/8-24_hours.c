03dde77fd6c5b8d8a197d6c4282dd05ea31826bf
Medmat0 <yassineelmtrour@gmail.com>
2023-07-18 15:44:29 -0700

    l

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*jack_bauer - print every minute form 00:00 to 23:59
+*
+*Return: nothing
+*/
+
+void jack_bauer(void)
+{
+
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+	}
+}
